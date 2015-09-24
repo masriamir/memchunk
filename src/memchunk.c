dfda42a0e0b1ed0716d7ef87db99d71107d76e0e
Amir Masri <masriamir@gmail.com>
2015-09-24 17:09:48 -0400

    Change function names

--- src/memchunk.c
+++ src/memchunk.c
@@ -6,7 +6,7 @@
 
 #include "memchunk.h"
 
-MemChunk *new_memchunk(const uint32_t size) {
+MemChunk *new_chunk(const uint32_t size) {
     MemChunk *mc;
 
     /* allocation memory for memchunk */
@@ -33,7 +33,7 @@ MemChunk *new_memchunk(const uint32_t size) {
     return mc;
 }
 
-bool free_data(MemChunk *const mc) {
+bool free_chunk_data(MemChunk *const mc) {
     /* check for valid data */
     if (mc->data) {
         free(mc->data);
@@ -44,10 +44,10 @@ bool free_data(MemChunk *const mc) {
     return false;
 }
 
-bool free_memchunk(MemChunk *mc) {
+bool free_chunk(MemChunk *mc) {
     /* check for valid memchunk */
     if (mc) {
-        free_data(mc);
+        free_chunk_data(mc);
 
         free(mc);
         mc = NULL;
@@ -58,14 +58,14 @@ bool free_memchunk(MemChunk *mc) {
     return false;
 }
 
-bool resize(MemChunk *mc, const uint32_t new_size) {
+bool resize_chunk(MemChunk *mc, const uint32_t new_size) {
     if (new_size < 1) {
         return false;
     }
 
     mc->data = realloc(mc->data, new_size);
     if (!mc->data) {
-        fprintf(stderr, "memchunk:resize - allocation of %u bytes failed\n", new_size);
+        fprintf(stderr, "memchunk:resize_chunk - allocation of %u bytes failed\n", new_size);
         return false;
     }
 
@@ -79,22 +79,22 @@ bool resize(MemChunk *mc, const uint32_t new_size) {
     return true;
 }
 
-bool clear(MemChunk *const mc) {
-    if (!is_empty(mc)) {
+bool clear_chunk(MemChunk *const mc) {
+    if (!is_chunk_empty(mc)) {
         mc->ptr = 0;
         mc->size = 0;
-        free_data(mc);
+        free_chunk_data(mc);
         return true;
     }
 
     return false;
 }
 
-bool is_empty(const MemChunk *mc) {
+bool is_chunk_empty(const MemChunk *mc) {
     return mc->size == 0 && !mc->data;
 }
 
-void seek_memchunk(MemChunk *const mc, const uint32_t offset, const uint32_t start) {
+void seek_chunk(MemChunk *const mc, const uint32_t offset, const uint32_t start) {
     if (start == SEEK_CUR) {
         /* offset based on current position */
         mc->ptr += offset;
@@ -117,7 +117,7 @@ void seek_memchunk(MemChunk *const mc, const uint32_t offset, const uint32_t sta
     }
 }
 
-bool read(void *const buf, MemChunk *const mc, const uint32_t size) {
+bool read_chunk(void *const buf, MemChunk *const mc, const uint32_t size) {
     /* valid destination and source data */
     if (!mc->data || !buf) {
         return false;
@@ -135,18 +135,18 @@ bool read(void *const buf, MemChunk *const mc, const uint32_t size) {
     return true;
 }
 
-bool read_ofs(void *const buf, MemChunk *const mc, const uint32_t size, const uint32_t offset) {
+bool read_chunk_ofs(void *const buf, MemChunk *const mc, const uint32_t size, const uint32_t offset) {
     /* make sure we are not reading past end of chunk */
     if (offset + size > mc->size) {
         return false;
     }
 
     /* seek to offset from beginning of chunk and then read as normal */
-    seek_memchunk(mc, offset, SEEK_SET);
-    return read(buf, mc, size);
+    seek_chunk(mc, offset, SEEK_SET);
+    return read_chunk(buf, mc, size);
 }
 
-bool write(MemChunk *mc, const void *data, const uint32_t size) {
+bool write_chunk(MemChunk *mc, const void *data, const uint32_t size) {
     /* valid data to write */
     if (!data) {
         return false;
@@ -154,7 +154,7 @@ bool write(MemChunk *mc, const void *data, const uint32_t size) {
 
     /* resize the memchunk if we are trying to write past the end of the chunk */
     if (mc->ptr + size > mc->size) {
-        resize(mc, mc->size + size);
+        resize_chunk(mc, mc->size + size);
     }
 
     /* copy the data and set the new position */
@@ -164,8 +164,8 @@ bool write(MemChunk *mc, const void *data, const uint32_t size) {
     return true;
 }
 
-bool write_ofs(MemChunk *mc, const void *data, const uint32_t size, const uint32_t offset) {
+bool write_chunk_ofs(MemChunk *mc, const void *data, const uint32_t size, const uint32_t offset) {
     /* seek to offset from beginning of chunk and then write as normal */
-    seek_memchunk(mc, offset, SEEK_SET);
-    return write(mc, data, size);
+    seek_chunk(mc, offset, SEEK_SET);
+    return write_chunk(mc, data, size);
 }
--- src/memchunk.h
+++ src/memchunk.h
@@ -10,19 +10,19 @@ typedef struct MemChunk {
     uint8_t *data;
 } MemChunk;
 
-MemChunk *new_memchunk(const uint32_t);
-bool free_data(MemChunk *const);
-bool free_memchunk(MemChunk *);
+MemChunk *new_chunk(const uint32_t);
+bool free_chunk_data(MemChunk *const);
+bool free_chunk(MemChunk *);
 
-bool resize(MemChunk *const, const uint32_t);
-bool clear(MemChunk *const);
-bool is_empty(const MemChunk *);
+bool resize_chunk(MemChunk *const, const uint32_t);
+bool clear_chunk(MemChunk *const);
+bool is_chunk_empty(const MemChunk *);
 
-void seek_memchunk(MemChunk *const, const uint32_t, const uint32_t);
+void seek_chunk(MemChunk *const, const uint32_t, const uint32_t);
 
-bool read(void *const, MemChunk *const, const uint32_t);
-bool read_ofs(void *const, MemChunk *const, const uint32_t, const uint32_t);
-bool write(MemChunk *, const void *, const uint32_t);
-bool write_ofs(MemChunk *, const void *, const uint32_t, const uint32_t);
+bool read_chunk(void *const, MemChunk *const, const uint32_t);
+bool read_chunk_ofs(void *const, MemChunk *const, const uint32_t, const uint32_t);
+bool write_chunk(MemChunk *, const void *, const uint32_t);
+bool write_chunk_ofs(MemChunk *, const void *, const uint32_t, const uint32_t);
 
 #endif
