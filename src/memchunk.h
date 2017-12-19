a938f0829c3b62daafc3d0a99262651fb0ef0472
Amir Masri <masriamir@gmail.com>
2017-12-19 14:55:43 -0500

    Format code using clang

--- src/memchunk.h
+++ src/memchunk.h
@@ -1,8 +1,8 @@
 #ifndef __MEMCHUNK_H__
 #define __MEMCHUNK_H__
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef struct MemChunk {
     uint32_t ptr;
@@ -10,22 +10,23 @@ typedef struct MemChunk {
     uint8_t *data;
 } MemChunk;
 
-MemChunk *new_chunk( const uint32_t );
-bool free_chunk_data( MemChunk *const );
-bool free_chunk( MemChunk * );
+MemChunk *new_chunk(const uint32_t);
+bool free_chunk_data(MemChunk *const);
+bool free_chunk(MemChunk *);
 
-bool resize_chunk( MemChunk *const, const uint32_t );
-bool clear_chunk( MemChunk *const );
-bool is_chunk_empty( const MemChunk * );
+bool resize_chunk(MemChunk *const, const uint32_t);
+bool clear_chunk(MemChunk *const);
+bool is_chunk_empty(const MemChunk *);
 
-void seek_chunk( MemChunk *const, const uint32_t, const uint32_t );
+void seek_chunk(MemChunk *const, const uint32_t, const uint32_t);
 
-bool read_chunk( void *const, MemChunk *const, const uint32_t );
-bool read_chunk_ofs( void *const, MemChunk *const, const uint32_t,
-                     const uint32_t );
+bool read_chunk(void *const, MemChunk *const, const uint32_t);
+bool read_chunk_ofs(void *const,
+                    MemChunk *const,
+                    const uint32_t,
+                    const uint32_t);
 
-bool write_chunk( MemChunk *, const void *, const uint32_t );
-bool write_chunk_ofs( MemChunk *, const void *, const uint32_t,
-                      const uint32_t );
+bool write_chunk(MemChunk *, const void *, const uint32_t);
+bool write_chunk_ofs(MemChunk *, const void *, const uint32_t, const uint32_t);
 
 #endif
